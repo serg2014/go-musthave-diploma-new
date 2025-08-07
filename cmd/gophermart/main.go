59c2218cb3ddf2501108a9d509380035e2f8bc11
Serg K <kozunov@example.com>
2025-08-08 00:31:25 +0300

    delete comment

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -105,33 +105,4 @@ func main() {
 
 	wg.Wait()
 	logger.Log.Info("Server is shutdown")
-
-	/*
-		// другой подход, тоже рабочий
-		go func() {
-			logger.Log.Info(fmt.Sprintf("Start server on %s", address))
-			err := srv.ListenAndServe()
-			if err != nil && !errors.Is(err, http.ErrServerClosed) {
-				logger.Log.Panic("error in ListenAndServe", zap.Error(err))
-			}
-			logger.Log.Info("Server is shutdown")
-		}()
-
-		// создаем контекст, который будет отменен при получении сигнала
-		ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-		defer stop()
-
-		// 	ждем сигнала от ОС
-		<-ctx.Done()
-		logger.Log.Info("catch signal")
-
-		// даем 5 секунд на завершение
-		// TODO время в конфиг
-		ctxT, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		if err := srv.Shutdown(ctxT); err != nil {
-			logger.Log.Info("Server forced to shutdown", zap.Error(err))
-		}
-		logger.Log.Info("Finish")
-	*/
 }
