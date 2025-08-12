0abc6ac73378a282f4e6e15457a2d8dfd7c6aeaf
Serg K <kozunov@example.com>
2025-08-12 13:35:11 +0300

    use timestamptz in db

--- migrations/000002_create_orders_table.up.sql
+++ migrations/000002_create_orders_table.up.sql
@@ -7,7 +7,7 @@ END $$;
 CREATE TABLE IF NOT EXISTS orders (
     order_id text NOT NULL PRIMARY KEY,
     user_id uuid NOT NULL,
-    upload_time timestamp NOT NULL DEFAULT current_timestamp,
+    upload_time timestamptz NOT NULL DEFAULT current_timestamp,
     status  order_status NOT NULL,
     accrual int
 );
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS orders_for_process (
     order_id text NOT NULL PRIMARY KEY,
     user_id uuid NOT NULL,
     who_lock char(20),
-    locked_at timestamp,
-    update_time timestamp NOT NULL
+    locked_at timestamptz,
+    update_time timestamptz NOT NULL
 );
 CREATE INDEX orders_for_process_who_lock_update_time_idx ON orders_for_process (who_lock, update_time);
 CREATE INDEX orders_for_process_locked_at_idx ON orders_for_process (locked_at);
